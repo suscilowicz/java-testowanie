9762d76e16b5626863acd804aa1434812c64eb13
suscilowicz <s.uscilowicz@gmail.com>
2016-03-02 16:05:05 +0100

    add project 'zad01'

---
+++ lab03/zad01/src/main/java/lab03/zad01/App.java
@@ -0,0 +1,13 @@
+package lab03.zad01;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
