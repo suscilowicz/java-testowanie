9250fdb69ec6feb4d393dad1a409875b80c874f9
suscilowicz <s.uscilowicz@gmail.com>
2016-03-05 18:42:15 +0100

    add zad02 and add interface "Psikus"

---
+++ lab03/zad02/zad02/src/main/java/lab03/zad02/Psikus.java
@@ -0,0 +1,9 @@
+package lab03.zad02;
+
+public interface Psikus {
+	int CyfroKrad(int liczba);
+	int HultajChochla(int liczba) throws NieudanyPsikusException;
+	int Nieksztaltek(int liczba);
+	int Heheszki (int liczba);
+	boolean Titit(int liczba_dziel);
+}
