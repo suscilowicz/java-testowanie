fc61e1af7f5dcf453c4861c8b19d936f3e00e749
suscilowicz <s.uscilowicz@gmail.com>
2016-03-07 17:48:02 +0100

    add CyfroKrad method in kontrakt.java

--- lab03/zad02/zad02/src/main/java/lab03/zad02/Kontrakt.java
+++ lab03/zad02/zad02/src/main/java/lab03/zad02/Kontrakt.java
@@ -1,10 +1,26 @@
 package lab03.zad02;
 
+import java.util.Random;
+
 public class Kontrakt implements Psikus {
+	Random random = new Random();
 
 	public Integer CyfroKrad(Integer liczba) {
-		// TODO Auto-generated method stub
-		return 0;
+		if (liczba.toString().length()<2) return null;
+		else if ((liczba.toString().length()<3) && (liczba.toString().equals("-"))) return null;
+		else {
+			if(liczba.toString().equals("-")){
+				String convert = liczba.toString();
+				int position = random.nextInt(convert.length()-1)+1;
+				return (Integer.parseInt(convert.substring(0,position)+convert.substring(position+1)));
+			
+				
+			} else {
+				String convert = liczba.toString();
+				int position = random.nextInt(convert.length());
+				return (Integer.parseInt(convert.substring(0,position)+convert.substring(position+1)));
+			}
+		}
 	}
 
 	public Integer Nieksztaltek(Integer liczba) {
