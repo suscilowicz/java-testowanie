e0bffe6e7b730a265ed252865bf2132691a8bdf3
suscilowicz <s.uscilowicz@gmail.com>
2016-03-22 22:47:57 +0100

    uzupelnienie mocka

--- messenger/src/main/java/com/example/mockdemo/messenger/MessageServiceMock.java
+++ messenger/src/main/java/com/example/mockdemo/messenger/MessageServiceMock.java
@@ -1,17 +1,35 @@
 package com.example.mockdemo.messenger;
 
 public class MessageServiceMock implements MessageService {
+	
+	private boolean sent = false;
+	private boolean connected = false;
+	
+	public void setSent(boolean bool) {
+		sent = bool;
+	}
+	
+	public void setConnected(boolean bool) {
+		connected = bool;
+	}
+	
 
 	@Override
 	public ConnectionStatus checkConnection(String server) {
-		// TODO Auto-generated method stub
-		return null;
+		if (!server.isEmpty() && server.endsWith(".pl") && connected) 
+			return ConnectionStatus.SUCCESS;
+		else return ConnectionStatus.FAILURE;
 	}
 
 	@Override
 	public SendingStatus send(String server, String message) throws MalformedRecipientException {
-		// TODO Auto-generated method stub
-		return null;
+		if (server.isEmpty() || message.isEmpty())
+			throw new MalformedRecipientException();
+		else if (server.length() < 4 || message.length() < 3)
+			throw new MalformedRecipientException();
+		else if (sent)
+			return SendingStatus.SENT;
+		else return SendingStatus.SENDING_ERROR;
 	}
 
 }
