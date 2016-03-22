66a6f00a2322bbf7b03f64d3c764a07173bcd5c4
suscilowicz <s.uscilowicz@gmail.com>
2016-03-22 22:22:05 +0100

    dodanie messageservicemock

---
+++ messenger/src/main/java/com/example/mockdemo/messenger/MessageServiceMock.java
@@ -0,0 +1,17 @@
+package com.example.mockdemo.messenger;
+
+public class MessageServiceMock implements MessageService {
+
+	@Override
+	public ConnectionStatus checkConnection(String server) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public SendingStatus send(String server, String message) throws MalformedRecipientException {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
